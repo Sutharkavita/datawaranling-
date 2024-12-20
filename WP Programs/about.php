6cddd8b9aa557ef8af6b497f19120a208a833ac9
kavitasuthar <159772686+Sutharkavita@users.noreply.github.com>
2024-12-20 17:21:20 +0530

    Add files via upload

---
+++ WP Programs/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us-ADARSH GROUP OF INSTITUTION</title>
+</head>
+<body>
+	<h1>About Us</h1>
+	<p>Founded in 1971, Adarsh Group of Institutions (AGI) is a reputed educational establishment 
+	   located at Chamarajpet in Bengaluru. The campus has a large built-up area with state-of-the-art 
+	   infrastructure for the students to receive high-quality management education at relatively affordable price.
+	</p>
+</body>
+</html>
---
+++ WP Programs/courses.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses-ADARSH GROUP OF INSTITUTION</title>
+</head>
+<body>
+	<h1>Courses</h1>
+	<h2>BCA</h2>
+	<h2>MCA</h2>
+	<h2>B.COM</h2>
+	<h2>BBA</h2>
+	<h2>BA</h2>
+	<h2>BSc</h2>
+</body>
+</html>
